66402f2c137058c8a79477d9f1133665184e1780
stefanmita444 <38542530+stefanmita444@users.noreply.github.com>
2024-11-29 17:24:02 -0600

    touch ups

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,11 +1,15 @@
 package com.mycompany.app;
 
-import software.constructs.Construct;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import imports.k8s.*;
 import org.cdk8s.App;
 import org.cdk8s.Chart;
 import org.cdk8s.ChartProps;
-import imports.k8s.*;
+import software.constructs.Construct;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -19,24 +23,24 @@ public class Main extends Chart {
     public Main(final Construct scope, final String id, final ChartProps props, final String appLabel, final String imageTag) {
         super(scope, id, props);
 
+
         Map<String, String> label = Collections.singletonMap("app", appLabel);
 
-        new KubeDeployment(this, "my-deployment", KubeDeploymentProps.builder()
+        new KubeDeployment(this, "tech-tonic-deployment", KubeDeploymentProps.builder()
                 .spec(DeploymentSpec.builder()
                         .replicas(3)
                         .selector(LabelSelector.builder()
                                 .matchLabels(label)
                                 .build())
                         .template(PodTemplateSpec.builder()
-                                .metadata(ObjectMeta.builder()
-                                        .labels(label)
+                                .metadata(ObjectMeta.builder().labels(label)
                                         .build())
                                 .spec(PodSpec.builder()
                                         .containers(List.of(Container.builder()
-                                                .name("app-container")
-                                                .image(imageTag) // Use the imageTag passed from context
+                                                .name("app-container-test")
+                                                .image(imageTag)
                                                 .ports(List.of(ContainerPort.builder()
-                                                        .containerPort(8080) // Adjust if needed
+                                                        .containerPort(80)
                                                         .build()))
                                                 .build()))
                                         .build())
@@ -45,7 +49,7 @@ public class Main extends Chart {
                 .build());
 
         // Optionally, define a Service to expose your application
-        new KubeService(this, "my-service", KubeServiceProps.builder()
+        new KubeService(this, "tech-tonic-service", KubeServiceProps.builder()
                 .spec(ServiceSpec.builder()
                         .type("LoadBalancer") // Expose externally
                         .selector(label)
@@ -57,14 +61,22 @@ public class Main extends Chart {
                 .build());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final App app = new App();
 
-        // Fetch imageTag and appLabel from context
-        String imageTag = (String) app.getNode().tryGetContext("image_tag");
-        String appLabel = "my-app"; // Or fetch from context if needed
+        ObjectMapper mapper = new ObjectMapper();
+        InputStream configStream = Main.class.getClassLoader().getResourceAsStream("image.json");
+        if (configStream == null) {
+            throw new RuntimeException("config.json not found in resources folder");
+        }
+        Map<String, String> image = mapper.readValue(configStream, new TypeReference<Map<String, String>>() {});
 
-        new Main(app, "getting-started", appLabel, imageTag);
+        String imageTag = image.getOrDefault("image_tag", "nginx:1.19.10"); // Default image
+        System.out.println("Using Docker Image: " + imageTag);
+
+        String appLabel = "tech-tonic-label";
+
+        new Main(app, "tech-tonic-id", appLabel, imageTag);
         app.synth();
     }
 }
